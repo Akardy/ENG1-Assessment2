39a66a12e869997a5dbb2cee478f069751c5eaf9
lolibaby666 <yw4684@york.ac.uk>
2024-12-05 18:27:30 +0000

    Update Piazza.java

--- core/src/main/java/com/badlogic/UniSim2/buildingmanager/Piazza.java
+++ core/src/main/java/com/badlogic/UniSim2/buildingmanager/Piazza.java
@@ -14,8 +14,8 @@ public class Piazza extends Building {
                 Assets.piazzaPlacedTexture,
                 Assets.piazzaCollisionTexture,
                 Assets.piazzaDraggingTexture,
-                Consts.LECTUREHALL_WIDTH,
-                Consts.LECTUREHALL_HEIGHT,
+                Consts.LECTUREHALL_WIDTH * 3 / 4,
+                Consts.LECTUREHALL_HEIGHT * 3 / 4,
                 BuildingTypes.Piazza);
     }
 
