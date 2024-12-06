8535e331546535811d1c905d25d1477032dfdf06
MatiasDuplock <Matias.duplock@gmail.com>
2024-12-06 02:11:16 +0000

    Finally Merged it and working...

--- core/src/main/java/com/badlogic/UniSim2/buildingmanager/Piazza.java
+++ core/src/main/java/com/badlogic/UniSim2/buildingmanager/Piazza.java
@@ -4,7 +4,7 @@ import com.badlogic.UniSim2.resources.*;
 
 /**
  * A building which represents a place where lectures can take place.
- * 
+ *
  * @see Building
  */
 public class Piazza extends Building {
@@ -14,8 +14,8 @@ public class Piazza extends Building {
                 Assets.piazzaPlacedTexture,
                 Assets.piazzaCollisionTexture,
                 Assets.piazzaDraggingTexture,
-                Consts.LECTUREHALL_WIDTH,
-                Consts.LECTUREHALL_HEIGHT,
+                Consts.LECTUREHALL_WIDTH * 3 / 4,
+                Consts.LECTUREHALL_HEIGHT * 3 / 4,
                 BuildingTypes.Piazza);
     }
 
